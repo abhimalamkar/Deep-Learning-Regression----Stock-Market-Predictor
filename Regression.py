81b361742981c78cd1987f0b59e11ca277e9293a
abhimalamkar <abhijeetmalamkar@gmail.com>
2016-11-26 20:46:37 -0500

    Graph
    Its Working but i still dont know how the date time is working ??
    
    Note:- Check datetime

--- Regression.py
+++ Regression.py
@@ -3,6 +3,12 @@ import quandl,math
 import numpy as np
 from sklearn import preprocessing ,cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+import datetime,arrow
+import time
+
+style.use('ggplot')
 
 #geting Data
 df = quandl.get('WIKI/GOOGL')
@@ -25,20 +31,48 @@ print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
+X = np.array(df.drop(['label'],1)) #features
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
 df.dropna(inplace=True)
 
-X = np.array(df.drop(['label'],1)) #features
-y = np.array(df['label'])  #labels
-
-X = preprocessing.scale(X)
 y = np.array(df['label'])
+y = np.array(df['label'])  #labels
 
 X_train,X_test,y_train,y_test = cross_validation.train_test_split(X,y,test_size = 0.2)
 
-clf = LinearRegression() #classifier
+clf = LinearRegression(n_jobs=-1) #classifier
 #clf = svm.SVR()
 clf.fit(X_train,y_train)
 
 accuracy = clf.score(X_test,y_test) #calculations
 
-print(accuracy)
+#print(accuracy)
+forecast_set = clf.predict(X_lately)
+
+print(forecast_set,accuracy,forecast_out)
+df['Forecast'] = np.nan
+#dates
+
+#not Sure
+#utc = arrow.utcnow()
+#local = utc.to('US/Pacific')
+last_date = df.iloc[-1].name
+last_unix = time.time()
+#last_unix = last_date.Timestamp() #correct line
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+       next_date = datetime.datetime.fromtimestamp(next_unix)
+       next_unix += one_day
+       df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
